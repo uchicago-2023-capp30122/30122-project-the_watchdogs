6efeb77b7c5a6a759258a745f49a9f87ccbea7f5
lkilpat <lkilpatrick@uchicago.edu>
2023-02-15 11:31:11 -0600

    finished scraper and add test files for cnn

--- cnn/scrape_cnn.py
+++ cnn/scrape_cnn.py
@@ -4,22 +4,10 @@ import lxml.html
 from lxml import etree
 import requests
 from utils import url_to_root
-from bs4 import BeautifulSoup
-
-#from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.keys import Keys
-
-from bs4 import BeautifulSoup
-html="""
-<p>hello <b>there</b></p>
-"""
-soup = BeautifulSoup(html, 'html.parser')
-p = soup.find('p')
-print(p.string)
-print(p.text)
 
 url="https://www.cnn.com/2022/02/09/politics/mitch-mcconnell-violent-insurrection/index.html"
+
+#when creating the crawler make sure the url does NOT include 'live-news'
 
 def scrape_article(url):
     """
@@ -45,14 +33,54 @@ def scrape_article(url):
     root = url_to_root(url)
 
     article['url'] = url
-    article['title'] = root.cssselect("h1.headline__text")[0].text_content().strip()
-    article['text'] = root.cssselect("div.article__content")[0].text_content().strip("\n")
+    article['title'] = root.cssselect("h1")[0].text_content().strip()
     article['source'] = 'CNN'
-    article['date'] = root.cssselect("div.timestamp")[0].text_content()
-    article['description'] = root.xpath("/html/head/meta[6]")
-    article['keywords'] = 'xx'
+    #check to see if this xpath is universal
+    article['description'] = root.xpath("/html/head/meta[6]/@content")[0]
+    
+    #change keywords to a list 
+    ##check to see if this xpath is universal
+    keywords_string = root.xpath("/html/head/meta[21]/@content")[0]
+    article['keywords'] = keywords_string.split(", ")
+    
+    #clean and add date
+    date = root.cssselect("div.timestamp")[0].text_content()
+    article['date'] = date.replace('\n', '').replace('  ', '').replace('Updated', '')
+
+    #clean and add body text
+    text = root.cssselect("div.article__content")[0].text_content().strip()
+    text = text.replace('\n', '').replace('  ', '')
+    article['text'] = text
+    #for mitch mcconnel article ... check to see if this in others
+    article['text'] = article['text'][13:]
+
 
     return article
+
+def articles_to_list(url_list):
+    """
+    Given a list of urls of articles, create a list of thier article dictionaries
+
+    Input: url_list (list): of urls 
+    
+    Returns: list of dictionaries
+    """
+
+    articles = []
+
+    for url in url_list:
+        articles.append(scrape_article(url))
+
+    return articles
+
+    #output is a list of articles
+
+def articles_to_json(articles):
+    """
+    """
+
+    with open("test_data/cnn_articles.json", "w") as f:
+        json.dump(articles, f, indent=1)
 
 
 
