a561f241bc5d4e93820fceaa8f650c76b1229083
rohitkandala <rohitk@uchicago.edu>
2023-03-04 15:20:19 -0600

    have first viz!

--- the_watchdogs/data_viz/app.py
+++ the_watchdogs/data_viz/app.py
@@ -1,36 +1,45 @@
 import sys
 from the_watchdogs.preprocess import preprocess
-from data_viz import create_dataframe, merge_dataframe
+from data_viz import create_sentiment
 import pathlib
-from wordcloud import WordCloud
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
 
-# WORDCLOUD:
+# SENTIMENT STACKED BAR CHART:
 
-# Load data from helper functions
-word_cnn, word_fox = create_dataframe()
+def plot_sentiment():
+    """
+    docstring
+    """
 
-# create the wordcloud
-wordcloud = WordCloud().generate_from_frequencies(word_frequencies)
+    cnn_count, fox_count = create_sentiment()
+    df = pd.concat([cnn_count, fox_count], axis=1)
+    df.columns = ["CNN", "FOX"]
 
-# create the Plotly figure
-fig = go.Figure(data=go.Image(z=wordcloud.to_array()))
+    # Defining the order of x-axis:
+    category_order = ["Very Negative", "Slightly Negative", "Neutral", "Slightly Positive", "Very Positive"]
 
-# set the layout
-layout = html.Div([
-    dcc.Graph(
-        id='wordcloud-graph',
-        figure=fig
-    )
-])
+    fig = px.bar(df, x=df.index, y=["CNN", "FOX"],
+                 barmode='group', labels={'value': 'Frequency'},
+                 category_orders={'index': category_order})
+    fig.update_layout(title='Sentiment Analysis by News Source', xaxis_title='Sentiment',
+                      yaxis_title='Number of Articles')
 
-# create the app
-app = Dash(__name__)
-app.layout = layout
+    return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    fig = plot_sentiment()
+    fig.show()
+
+
+# MONTH/YEAR LINE GRAPH
+
+def plot_month_year():
+    """
+    docstring
+    """
+    
+    pass
