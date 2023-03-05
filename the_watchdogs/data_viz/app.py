2cfb881feab8e8b07eba325ff55999713bc21022
rohitkandala <rohitk@uchicago.edu>
2023-03-04 22:17:22 -0600

    inital draft working!

--- the_watchdogs/data_viz/app.py
+++ the_watchdogs/data_viz/app.py
@@ -1,45 +1,6 @@
 import sys
-from the_watchdogs.preprocess import preprocess
-from data_viz import create_sentiment
-import pathlib
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
-
-# SENTIMENT STACKED BAR CHART:
-
-def plot_sentiment():
-    """
-    docstring
-    """
-
-    cnn_count, fox_count = create_sentiment()
-    df = pd.concat([cnn_count, fox_count], axis=1)
-    df.columns = ["CNN", "FOX"]
-
-    # Defining the order of x-axis:
-    category_order = ["Very Negative", "Slightly Negative", "Neutral", "Slightly Positive", "Very Positive"]
-
-    fig = px.bar(df, x=df.index, y=["CNN", "FOX"],
-                 barmode='group', labels={'value': 'Frequency'},
-                 category_orders={'index': category_order})
-    fig.update_layout(title='Sentiment Analysis by News Source', xaxis_title='Sentiment',
-                      yaxis_title='Number of Articles')
-
-    return fig
-
-if __name__ == '__main__':
-    fig = plot_sentiment()
-    fig.show()
-
-
-# MONTH/YEAR LINE GRAPH
-
-def plot_month_year():
-    """
-    docstring
-    """
-    
-    pass
--- the_watchdogs/data_viz/data_viz.py
+++ the_watchdogs/data_viz/data_viz.py
@@ -1,13 +1,11 @@
 import sys
-from the_watchdogs.preprocess import preprocess
 import pathlib
-from wordcloud import WordCloud
 import pandas as pd
-import plotly.express as px
-from dash import Dash, html, dcc
+import numpy as np
 from collections import Counter
 
-# GENERAL HELPER FUNCTIONS:
+# Below are the some functions that take the pre-process data, and turn the
+# csvs into pandas series to be used in "" to visualize the data:
 
 # Loading in csv files as global variables and saving them as global
 # dataframes:
@@ -17,52 +15,26 @@ fox_data = pathlib.Path(__file__).parent / "../data/FOX_clean_df.csv"
 df_cnn = pd.read_csv(cnn_data)
 df_fox = pd.read_csv(fox_data)
 
-def create_dataframe():
-    """
-    docstring
-    """
-
-    cnn_data = pathlib.Path(__file__).parent / "../data/CNN_clean_df.csv"
-    fox_data = pathlib.Path(__file__).parent / "../data/FOX_clean_df.csv"
-
-    df_cnn = pd.read_csv(cnn_data).clean_text.str.replace('[','').str.replace(']','').str.replace("'",'')
-    df_fox = pd.read_csv(fox_data).clean_text.str.replace('[','').str.replace(']','').str.replace("'",'')
-
-    df_cnn = df_cnn.apply(lambda x: x.split(', '))
-    df_fox = df_fox.apply(lambda x: x.split(', '))
-
-    cnn_count = []
-    for i in df_cnn.values:
-        cnn_count += i
-    fox_count = []
-    for i in df_fox.values:
-        fox_count += i
-
-    cnn_count = pd.Series(Counter(cnn_count)).sort_values(ascending = False)
-    fox_count = pd.Series(Counter(fox_count)).sort_values(ascending = False)
-    return cnn_count, fox_count
-
 
 def word_count():
     """
     docstring
     """
-
-    text_cnn = df_cnn.clean_text.str.replace('[','').str.replace(']','').str.replace("'",'')
-    text_fox = df_fox.clean_text.str.replace('[','').str.replace(']','').str.replace("'",'')
-
-    text_cnn = text_cnn.apply(lambda x: x.split(', '))
-    text_fox = text_fox.apply(lambda x: x.split(', '))
+    # text_cnn = df_cnn.clean_text.str.replace('[','').str.replace(']','').str.replace("'",'')
+    # text_fox = df_fox.clean_text.str.replace('[','').str.replace(']','').str.replace("'",'')
+    # text_cnn = text_cnn.apply(lambda x: x.split(', '))
+    # text_fox = text_fox.apply(lambda x: x.split(', '))
 
     cnn_count = []
-    for i in text_cnn.values:
-        cnn_count += i
+    for i in df_cnn.clean_text.values:
+        cnn_count += eval(i)
     fox_count = []
-    for i in text_fox.values:
-        fox_count += i
+    for i in df_fox.clean_text.values:
+        fox_count += eval(i)
 
     cnn_count = pd.Series(Counter(cnn_count)).sort_values(ascending = False)
     fox_count = pd.Series(Counter(fox_count)).sort_values(ascending = False)
+
     return cnn_count, fox_count
 
 
@@ -74,15 +46,35 @@ def create_date():
     text_cnn = df_cnn.date.apply(lambda x: x.split(','))
     text_fox = df_fox.date.apply(lambda x: x.split(','))
 
-    text_cnn = text_cnn.apply(lambda x: (x[0].split(' ')[0], x[1][:5]))
-    text_fox = text_fox.apply(lambda x: (x[0].split(' ')[0], x[1][:5]))
+    text_cnn = text_cnn.apply(lambda x: (x[0].split(' ')[0], int(x[1][1:5])))
+    text_fox = text_fox.apply(lambda x: (x[0].split(' ')[0], int(x[1][1:5])))
+    cnn_count = pd.Series(Counter(text_cnn)).reset_index().set_index('level_0')
+    fox_count = pd.Series(Counter(text_fox)).reset_index().set_index('level_0')
 
-    cnn_count = pd.Series(Counter(text_cnn)).sort_values(ascending = False)
-    fox_count = pd.Series(Counter(text_fox)).sort_values(ascending = False)
-    #  pd.concat([cnn_count, fox_count], axis=1).dropna()
-    #  cnn_count, fox_count.loc[cnn_count.index]
-    return cnn_count, fox_count
+    months = ['January','February','March','April','May','July','August','September','October','November','December']
+    ind_cnn = cnn_count.level_1
+    ind_fox = fox_count.level_1
+    cnn_count = cnn_count[0]
+    fox_count = fox_count[0]
+    years = np.union1d(ind_cnn.unique(), ind_fox.unique())
 
+    res = []
+    for y in years:
+        temp_cnn = cnn_count[ind_cnn == y].rename(f"CNN {y}")
+        if len(temp_cnn) != 0:
+            temp_cnn = temp_cnn.reindex(index = months)
+            res.append(temp_cnn)
+
+    for y in years:
+        temp_fox = fox_count[ind_fox == y].rename(f"FOX {y}")
+        if len(temp_fox) != 0:
+            temp_fox = temp_fox.reindex(index = months)
+            res.append(temp_fox)
+
+    result = pd.concat(res, axis=1)
+    result.index.name = 'Date'
+    result.reset_index()
+    return result
 
 def create_sentiment():
     """
@@ -94,5 +86,5 @@ def create_sentiment():
 
     cnn_count = pd.Series(Counter(text_cnn)).sort_values(ascending = False)
     fox_count = pd.Series(Counter(text_fox)).sort_values(ascending = False)
-    
+
     return cnn_count, fox_count
